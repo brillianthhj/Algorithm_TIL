8db6b29a83736aa39ffa49937bac8210e8768a6a
brillianthhj <brillianthhj@gmail.com>
2023-07-16 18:39:21 +0900

    move folders

--- Week3/Q3.Top_K_Frequent_Elements.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter, OrderedDict
-
-class Solution:
-    def topKFrequent(self, nums, k):
-        d = Counter(nums)
-        dd = dict(OrderedDict(sorted(d.items(), key = lambda t : t[1], reverse = True)))
-        
-        return list(dd.keys())[:k]
